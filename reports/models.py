57fa796d433558c61ca2d29ed69e81c30a313717
IvanPrudey <prudey.ia@yandex.ru>
2025-10-24 20:56:37 +0300

    add_model_User

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from constants.constants import (
+    ROLE_CHOICES
+)
+
+
+class User(AbstractUser):
+
+    telegram_id = models.BigIntegerField(
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name='ID в Telegram',
+        help_text='Уникальный идентификатор пользователя в Telegram'
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='user',
+        verbose_name='Роль',
+    )
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        verbose_name='Телефон'
+    )
+    is_verified = models.BooleanField(
+        default=False,
+        verbose_name='Подтвержден'
+    )
+    last_activity = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Последняя активность'
+    )
