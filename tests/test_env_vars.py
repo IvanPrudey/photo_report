fa3f9ec80d3841b7b5452ea66365aac6fe834fd9
IvanPrudey <prudey.ia@yandex.ru>
2026-02-14 11:24:58 +0300

    correct_test_user_creation_with_correct_fields

--- tests/test_env_vars.py
+++ tests/test_env_vars.py
@@ -5,7 +5,7 @@ class TestRequiredEnvVars():
 
     def test_required_env_vars_exist(self):
         '''Проверка наличия обязательных переменных и их значений.'''
-        required_vars = ['SECRET_KEY', 'BOT_TOKEN', 'DEBUG']
+        required_vars = ['SECRET_KEY', 'BOT_TOKEN']
         for var_name in required_vars:
             assert os.getenv(var_name) is not None, f'Переменная {var_name} не установлена'
             assert os.getenv(var_name) != '', f'Переменная {var_name} пустая'
